db3714ad312a1a09bfd1bdc232f1de6e9f5f23ff
aryan <n01393003@humbermail.ca>
2021-10-10 14:23:39 -0400

    Test commit

--- app/src/main/java/ca/codingcomrades/it/buscareplus/MainActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/MainActivity.java
@@ -28,7 +28,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import ca.codingcomrades.it.buscareplus.databinding.ActivityMainBinding;
 import ca.codingcomrades.it.buscareplus.ui.HelpActivity;
-
+// A test comment
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
