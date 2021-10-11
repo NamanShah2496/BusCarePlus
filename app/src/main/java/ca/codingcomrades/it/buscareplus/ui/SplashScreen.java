67d395d0a2e1ed5626f98b76715f25d848d0d5a9
aryan <n01393003@humbermail.ca>
2021-10-11 12:12:29 -0400

    Initial Commit to add the files required for Splashscreen design.

---
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/ui/SplashScreen.java
@@ -0,0 +1,10 @@
+// Section RNA
+// Naman Shah , n01392496 , Section RNA
+// Aryan Sood , n01393003, Section RNA
+// Vishesh Bansal, n01395119, Section RNA
+// Jaskirat Singh , N01403975 , Section RNB
+
+package ca.codingcomrades.it.buscareplus.ui;
+
+public class SplashScreen {
+}
