ee03cbd5fd71d0b22a4ecbf8c26084e50409815f
Vishesh <visheshbansal369@gmail.com>
2021-10-12 19:48:04 -0400

    Included Landscape layout for Login Screen

--- app/src/main/java/ca/codingcomrades/it/buscareplus/LoginActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/LoginActivity.java
@@ -3,19 +3,27 @@ package ca.codingcomrades.it.buscareplus;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
+        login = findViewById(R.id.Login_btn);
+        login.setOnClickListener(v-> callHome());
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
 
         }
+    }
+    public void callHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
