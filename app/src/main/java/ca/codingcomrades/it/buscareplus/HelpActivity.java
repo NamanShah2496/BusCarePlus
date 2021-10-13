09115bebeaa251fc07c40b7bac05842160b7e956
naman0301 <65423813+naman0301@users.noreply.github.com>
2021-10-12 20:17:00 -0400

    Help page and home screen icon updated with addition of menu items in drawer as well as side menu

--- app/src/main/java/ca/codingcomrades/it/buscareplus/HelpActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/HelpActivity.java
@@ -6,7 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -21,6 +21,10 @@ import org.jetbrains.annotations.NotNull;
 
 public class HelpActivity extends AppCompatActivity {
 
+    public View view;
+    Button button;
+    public static final int REQUEST_PHONE_CALL = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +35,39 @@ public class HelpActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
 
         }
+        button = findViewById(R.id.button2);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//               // insertDummyContactWrapper();
+//            }
+//        });
 
     }
+//    public void insertDummyContactWrapper() {
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+//            Intent callIntent = new Intent();
+//            callIntent.setAction(Intent.ACTION_CALL);
+//            callIntent.setData(Uri.parse("TEL:" + "+1 437-235-6780"));
+//            startActivity(callIntent);
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.CALL_PHONE},  REQUEST_PHONE_CALL);
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == REQUEST_PHONE_CALL && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+//        } else if (requestCode != REQUEST_PHONE_CALL && grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+//            ConstraintLayout text = findViewById(R.id.constraintLayout);
+//            Snackbar snackbar = Snackbar
+//                    .make(text, "Permission Denied", Snackbar.LENGTH_LONG);
+//            snackbar.show();
+//        }
+//    }
+
 
 }
