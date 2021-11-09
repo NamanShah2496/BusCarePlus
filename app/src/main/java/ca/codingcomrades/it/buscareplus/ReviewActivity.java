df217ece12bee2109f48304f56c144974d2a6e09
aryan <n01393003@humbermail.ca>
2021-11-08 23:03:12 -0500

    Basic structure of the review screen

--- app/src/main/java/ca/codingcomrades/it/buscareplus/ReviewActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/ReviewActivity.java
@@ -1,6 +1,23 @@
 package ca.codingcomrades.it.buscareplus;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReviewActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        EditText nameEditText = findViewById(R.id.username_input);
+        String fullName = nameEditText.getText().toString();
+        TextView tv1 = findViewById(R.id.model_print);
+        String str = android.os.Build.MODEL;
+        tv1.setText(str);
+    }
+
 }
+
