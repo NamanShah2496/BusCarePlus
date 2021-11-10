ffa80ea17c6e8c998be8d19dd57453f481ccd511
Vishesh <visheshbansal369@gmail.com>
2021-11-10 01:02:02 -0500

    Binding datafields of Review Screen and including Firebase dependencies

--- app/src/main/java/ca/codingcomrades/it/buscareplus/ReviewActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/ReviewActivity.java
@@ -1,22 +1,68 @@
 package ca.codingcomrades.it.buscareplus;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ReviewActivity  extends AppCompatActivity {
+import com.google.firebase.database.FirebaseDatabase;
 
+
+public class ReviewActivity  extends AppCompatActivity {
+FirebaseDatabase database;
+Button submit;
+RatingBar ratingBar;
+TextView model;
+EditText fullName,phone,email,comment;
+    Float rating;
+    String name,num,emailAddress,Comment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
-       // EditText nameEditText = (EditText) findViewById(R.id.username_input);
-        //String fullName = nameEditText.getText().toString();
-        TextView tv1 = (TextView) findViewById(R.id.model_print);
+        bindFields();
+        submit.setOnClickListener(v -> sendReview());
         String str = android.os.Build.MODEL;
-        tv1.setText(str);
+        model.setText(str);
     }
+    public void sendReview(){
+        if(validate()) {
+           database = FirebaseDatabase.getInstance();
+            Log.d("TAG", "sendReview: Working" );
+//            DatabaseReference myRef = database.getReference("FeedBack/Speed");
+//            myRef.setValue("12Km/h");
+//        fStore = FirebaseFirestore.getInstance();
+//        fAuth = FirebaseAuth.getInstance();
+        }
+    }
+    public void bindFields(){
+        fullName = findViewById(R.id.fullNameInput);
+        phone = findViewById(R.id.phoneNumInput);
+        email = findViewById(R.id.emailInput);
+        ratingBar =findViewById(R.id.ratingBar);
+        comment = findViewById(R.id.commentInput);
+        model = findViewById(R.id.model_print);
+        submit = findViewById(R.id.submitBtn);
 
+    }
+    public boolean validate(){
+        getValues();
+        if(name.isEmpty() || num.isEmpty() || emailAddress.isEmpty() || Comment.isEmpty() || rating==0){
+            return false;
+        }
+        return true;
+    }
+    public void getValues(){
+        name = fullName.getText().toString();
+        num = phone.getText().toString();
+        emailAddress = email.getText().toString();
+        Comment = comment.getText().toString();
+        rating = ratingBar.getRating();
+        Log.d("TAG", "getValues: " +rating);
+
+    }
 }
