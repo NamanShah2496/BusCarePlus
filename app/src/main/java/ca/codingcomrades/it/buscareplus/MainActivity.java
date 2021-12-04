cebce5fed234da9323eaa1746918eab7947e9cf5
AryanSood3003 <84537277+AryanSood3003@users.noreply.github.com>
2021-12-04 13:35:27 -0500

    Background Notification bug fix

--- app/src/main/java/ca/codingcomrades/it/buscareplus/MainActivity.java
+++ app/src/main/java/ca/codingcomrades/it/buscareplus/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
     }
     //Behavioral Patterns
 //Command Design Pattern
